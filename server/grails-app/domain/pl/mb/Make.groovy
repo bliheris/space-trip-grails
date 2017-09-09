d8536c82b4eeed4bf21c7a645a66934d7ad9ad21
Maciej Bołtromiuk <maciejboltromiuk@gmail.com>
2017-09-09 10:52:16 +0200

    Added spring security annotations

--- server/grails-app/domain/pl/mb/Make.groovy
+++ server/grails-app/domain/pl/mb/Make.groovy
@@ -1,7 +1,11 @@
 package pl.mb
 
+import grails.plugin.springsecurity.annotation.Secured
 import grails.rest.Resource
+import groovy.transform.CompileStatic
 
+@CompileStatic
+@Secured(['ROLE_DRIVER'])
 @Resource(uri = '/make')
 class Make {
 
