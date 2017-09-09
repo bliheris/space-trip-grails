9b1cf4d604a619e2872a74c11b4a652cfbc09b47
Maciej Bołtromiuk <maciejboltromiuk@gmail.com>
2017-09-09 09:17:23 +0200

    AddVehicleForm

--- client/src/garage.js
+++ client/src/garage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'whatwg-fetch';
 
 import Vehicles from './vehicles';
+import AddVehicleForm from './AddVehicleForm'
 
 
 class Garage extends React.Component {
@@ -10,23 +11,60 @@ class Garage extends React.Component {
         super();
 
         this.state = {
-            vehicles: []
+            vehicles: [],
+            makes: [],
+            models: [],
+            drivers: [],
         }
     }
+
+    submitNewVehicle = vehicle => {
+        fetch('http://localhost:8080/vehicle', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(vehicle)
+        }).then(r => r.json())
+            .then(json => {
+                let vehicles = this.state.vehicles;
+                vehicles.push({id: json.id, name: json.name, make: json.make, model: json.model, driver: json.driver});
+                this.setState({vehicles});
+            })
+            .catch(ex => console.error('Unable to save vehicle', ex));
+    };
 
     componentDidMount() {
         fetch('http://localhost:8080/vehicle')
             .then(r => r.json())
             .then(json => this.setState({vehicles: json}))
             .catch(error => console.error('Error retrieving vehicles: ' + error));
+
+        fetch('http://localhost:8080/make')
+            .then(r => r.json())
+            .then(json => this.setState({makes: json}))
+
+        fetch('http://localhost:8080/model')
+            .then(r => r.json())
+            .then(json => this.setState({models: json}))
+
+        fetch('http://localhost:8080/driver')
+            .then(r => r.json())
+            .then(json => this.setState({drivers: json}))
     }
 
     render() {
-        const {vehicles} = this.state;
+        const {vehicles, makes, models, drivers} = this.state;
 
-        return <div>
-            <Vehicles vehicles={vehicles} />
-        </div>;
+        return (
+            <div>
+                <AddVehicleForm
+                    onSubmit={this.submitNewVehicle}
+                    makes={makes}
+                    models={models}
+                    drivers={drivers}
+                />
+                <Vehicles vehicles={vehicles} />
+            </div>
+        )
     }
 }
 
